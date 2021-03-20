3496aa3c97650663946431148f97432ec81f21a8
ekin_serttas <ekin.serttas@bosphorusiss.com>
2021-03-20 10:40:28 +0300

    pom model is implemented

--- src/main/java/TyHomePage.java
+++ src/main/java/TyHomePage.java
@@ -1,21 +1,37 @@
-
 import com.codeborne.selenide.*;
-
-
+import org.openqa.selenium.Keys;
+import java.util.List;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+public class TyHomePage extends BasePage {
 
-public class TyHomePage extends BasePage{
-    private SelenideElement searchField = $(".search-box-container").find(".search-box");
-    private SelenideElement PopUpClose =$(".fancybox-item.fancybox-close");
-    public void closePopUp() {
-        PopUpClose.click();
-
+    private List<SelenideElement> listResult = $$(".prdct-desc-cntnr-name");
+    private SelenideElement txtSearch = $(".search-box-container").find(".search-box");
+    private SelenideElement btnClosePopup = $(".fancybox-item.fancybox-close");
+    private SelenideElement chkFilterMan = $$(".fltr-item-wrppr").findBy(text("Erkek"));
+    private SelenideElement lblDescription = $(".dscrptn");
+    public TyHomePage(){
+        super();
+        open("http://www.trendyol.com/");
     }
 
-    //send Sateen
-    public void enterSearchTerm(String text) {
-        searchField.val(text).pressEnter();
+    public void clickClosePopup() {
+        btnClosePopup.click();
     }
-
-
+    public void setSearchText(String searchText) {
+        txtSearch.sendKeys(searchText);
+        txtSearch.sendKeys(Keys.ENTER);
+    }
+    public void clickFilterMan() {
+        chkFilterMan.click();
+    }
+    public List<SelenideElement> getListResult() {
+        return listResult;
+    }
+    public void clickDescription() {
+        lblDescription.doubleClick();
+    }
+    public void wait(int millisecond) throws InterruptedException {
+        Thread.sleep(millisecond);
+    }
 }
--- src/test/java/FilterCheck.java
+++ src/test/java/FilterCheck.java
@@ -1,59 +1,35 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.testng.ScreenShooter;
-import extensions.RetryAnalyzer;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-
 import java.util.List;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
-
-@Listeners({ ScreenShooter.class })
+import static com.codeborne.selenide.Selenide.open;
+@Listeners({ScreenShooter.class})
 public class FilterCheck {
-    @Test()
-    public void searchbook() throws InterruptedException {
-
-        Configuration.browser = "Chrome";
-        Configuration.startMaximized = true;
-        //Configuration.holdBrowserOpen = true;
-        //open Trendyol
-        open("http://www.trendyol.com/");
-
-        $(".fancybox-item.fancybox-close").click();
-        new TyHomePage();
-        //search Sateen
-        $(".search-box-container").find(".search-box").sendKeys("Sateen");
-        $(".search-box-container").find(".search-icon").click();
-
-        //Filter man
-        checkClickable(2);
-        //Thread.sleep(6000);
-        //List a = $$(".prdct-desc-cntnr-name");
-        List<SelenideElement> titles =  $$(".prdct-desc-cntnr-name");
-        for(SelenideElement element : titles)
-        {
-            Assert.assertFalse(element.text().contains("Kadın"),"Erkek filtresinde kadın ürünü olmamalı.");
-        }
 
 
+    TyHomePage page = new TyHomePage();
+    @Test
+    public void searchSateen() throws InterruptedException {
 
-    }
-
-    public void checkClickable(int try_no){
-        for (int i=0; i<try_no;i++ ) {
-            try {
-                $$(".fltr-item-wrppr").findBy(text("Erkek")).click();
+        page.clickClosePopup();
+        page.setSearchText("Sateen");
+        page.clickDescription();
+        page.wait(3000);
+        page.clickDescription();
+        page.clickFilterMan();
+        List<SelenideElement> titles = page.getListResult();
+        boolean isTitle = false;
+        for (SelenideElement element : titles) {
+            if (element.text().contains("Kadın")) {
+                isTitle = true;
                 break;
-            } catch (Exception e ) {
-                $(".dscrptn").click();
             }
         }
+        Assert.assertTrue(isTitle, "Filtreleme için Erkek seçili olmasına rağmen Kadın ürünleri de listelendi.");
     }
-
-
-
-}
+    }
